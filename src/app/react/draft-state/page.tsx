dd3f7fd9eeaf7e8955f9f22dd8c4ed62df8c97d4
raehan <raehan900809@gmail.com>
2024-03-30 15:40:14 +0900

    chore: draft state 확인을 위한 기본 코드 작성

---
+++ src/app/react/draft-state/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React, {useState} from 'react';
+
+const Page = () => {
+  const [state, setState] = useState();
+
+  return (
+    <div>
+      draft state
+    </div>
+  );
+};
+
+export default Page;
