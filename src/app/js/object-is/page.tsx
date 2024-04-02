b21ae7a7ecb2df5f7705546f4042a0aaa4f403b2
raehan <raehan900809@gmail.com>
2024-04-02 11:25:33 +0900

    chore: deep props 비교도 하는 방법 추가.

--- src/app/js/object-is/page.tsx
+++ src/app/js/object-is/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, {memo, useEffect, useState} from 'react';
+import React, {memo, useEffect, useMemo, useState} from 'react';
 
 type Props = {
   counter: number;
@@ -20,6 +20,8 @@ const Component = memo(({ counter }: Props) => {
   return <h3>{counter}</h3>
 })
 
+
+
 const DeeperComponent = memo(({ counter }: DeepProps) => {
   useEffect(() => {
     console.log('DeeperComponent has been rendered!!');
@@ -35,11 +37,18 @@ const Page = () => {
     setCounter(prevState => prevState + 1);
   }
 
+  const counter = useMemo(() => {
+    return ({
+      counter: 100
+    })
+  }, []);
+
   return (
     <div>
       <h2>normal props component vs deep props component</h2>
       <Component counter={100} />
-      <DeeperComponent counter={{ counter: 100 }} />
+      {/*<DeeperComponent counter={{ counter: 100 }} />*/}
+      <DeeperComponent counter={counter} />
       <button onClick={handleClick}>Click me!</button>
     </div>
   );
